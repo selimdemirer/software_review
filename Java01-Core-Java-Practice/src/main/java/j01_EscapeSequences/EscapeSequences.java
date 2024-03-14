b3ce8936d015e148f0ae51932f7a75508a4cf428
selimdemirer <selimmdemirerr@gmail.com>
2024-03-14 17:07:20 +0300

    Escape Sequences is done

---
+++ Java01-Core-Java-Practice/src/main/java/j01_EscapeSequences/EscapeSequences.java
@@ -0,0 +1,35 @@
+package j01_EscapeSequences;
+/*
+Escape Sequences: MUST be given within double quote
+
+  \n:Starts a new line
+  \t:Paragraph Space (Tab)
+  \\:Backslash
+  \":Double Quote
+
+ */
+public class EscapeSequences {
+    public static void main(String[] args) {
+
+        System.out.println("Java \nPython \nC#");
+
+        System.out.println("---------------------------------------------");
+
+        System.out.println("Hello Cydeo \n\nHow are you all? \nIt's nice to see you all! \nWhat class do we have next week?");
+
+        System.out.println("-----------------------------------------------");
+
+        System.out.println("\tJava is Cool Programming Language");
+
+        System.out.println("----------------------------------------");
+
+        System.out.println("/ \\\\"); //If you need 1 backslash, you need to type 2
+
+        System.out.println("---------------------------------------");
+
+        System.out.println("My favourite TV show is \"Patriot Act\"");
+    }
+
+
+
+}
---
+++ Java01-Core-Java-Practice/src/main/java/j01_EscapeSequences/Triangle.java
@@ -0,0 +1,26 @@
+package j01_EscapeSequences;
+
+public class Triangle {
+
+    public static void main(String[] args) {
+
+        System.out.println("\t\t\t     ^");
+        System.out.println("\t\t\t    / \\");
+        System.out.println("\t\t\t   /   \\");
+        System.out.println("\t\t\t  /     \\");
+        System.out.println("\t\t\t /       \\");
+        System.out.println("\t\t\t ---------");
+
+    }
+}
+/*
+Create a class called Triangle.java and print:
+
+                    ^
+                   / \
+                  /   \
+                 /     \
+                /       \
+                ---------
+
+ */
