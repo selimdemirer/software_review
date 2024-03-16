6570a250992764edd047d4fdec823fbc50fde8ba
selimdemirer <selimmdemirerr@gmail.com>
2024-03-16 14:17:16 +0300

    Concatenation is done

---
+++ Java01-Core-Java-Practice/src/main/java/j03_Concatenation/Introduction.java
@@ -0,0 +1,17 @@
+package j03_Concatenation;
+
+public class Introduction {
+
+    public static void main(String[] args) {
+
+        String name = "Selim";
+        int age = 37;
+
+        // Hello Everyone, my name is _____, and I am __ years old.
+
+        System.out.println("Hello Everyone, my name is " + name + ", and I am " + age + " years old.");
+
+    }
+
+
+}
