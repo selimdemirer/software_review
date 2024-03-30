feea60afc3ea92ea13e024698083dedf5a69263b
selimdemirer <selimmdemirerr@gmail.com>
2024-03-30 16:19:50 +0300

    Arrays is started

---
+++ Java01-Core-Java-Practice/src/main/java/j14_Arrays/Alphabet.java
@@ -0,0 +1,28 @@
+package j14_Arrays;
+
+import java.util.Arrays;
+
+public class Alphabet {
+
+    public static void main(String[] args) {
+
+        char[] alphabets = new char[26]; // 26 represents number of elements, not indexes!
+        char ch = 'Z';
+
+        for (char i = 0; i <alphabets.length ; i++, ch--) {
+            alphabets[i] = ch; // Z~A
+        }
+
+        /*
+        for (char i = 0, j = 'Z'; i <alphabets.length; i++,j--) {
+            alphabets[i]=j;
+        }
+        */
+
+        System.out.println(Arrays.toString(alphabets)); // print the whole array
+        System.out.println(alphabets[5]); // 5 represents index, not element! Print the element of array: 6. element!
+        System.out.println(alphabets);
+
+
+    }
+}
---
+++ Java01-Core-Java-Practice/src/main/java/j14_Arrays/UniqueElements.java
@@ -0,0 +1,40 @@
+package j14_Arrays;
+
+public class UniqueElements {
+
+    public static void main(String[] args) {
+
+        String[] words = {"Java", "Java", "C#", "Python", "Python"};
+
+        for (int j = 0; j < words.length; j++) {
+
+            String element = words[j]; // "Java"
+            int frequency = 0;
+            for (int i = 0; i < words.length; i++) { // finds the frequency of element from array
+                if (words[i].equals(element)) {
+                    frequency++;
+                }
+            }
+
+            if (frequency == 1){
+                System.out.println(element);
+            }
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
