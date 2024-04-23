584964e604bd39d6dbac6758e28fab61f9b8d5cb
selimdemirer <selimmdemirerr@gmail.com>
2024-04-23 13:44:31 +0300

    Custom Class is done

---
+++ Java01-Core-Java-Practice/src/main/java/j23_CustomClass/Class_vs_Object.java
@@ -0,0 +1,42 @@
+package j23_CustomClass;
+
+public class Class_vs_Object {
+
+/*
+Class:
+• Where Objects came from
+• An entity that determines how an object will behave and what the object will contain
+• A blueprint or set of instruction to build a specific type of Object
+
+Object:
+• Instance of a class
+• An object can store data
+• The data stored in an object are called fields
+• Multiple objects can be created from a class
+
+Class                                       Object
+Class is a collection of similar objects    Object is an instance of a class
+Class is conceptual (is a template)         Object is real
+No memory is allocated for a class          Each object has its own memory
+Class can exist without any objects         Objects can not exist without a class
+
+Instance Variables:
+• Declared inside the class and outside any methods/blocks
+• Belongs to the object, and each object has its own memory
+• Each Object has a separate copy of the instance variable
+
+Instance Methods:
+• Does not have static keyword
+• Belongs to the object
+• Each Object has a separate copy of the instance methods
+
+Creating an Object/Instance:
+• The new keyword is used to create an instance of a class
+
+Accessing an object’s Data and Methods:
+• An Object’s members refer to it’s data fields and methods. After object is created its
+data can be accessed and its methods can be invoked using the dot operator (.)
+
+*/
+
+}
--- Java01-Core-Java-Practice/src/main/java/j23_CustomClass/DogObjects.java
+++ Java01-Core-Java-Practice/src/main/java/j23_CustomClass/DogObjects.java
@@ -1,8 +1,6 @@
 package j23_CustomClass;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.stream.Collectors;
 
 public class DogObjects {
 
