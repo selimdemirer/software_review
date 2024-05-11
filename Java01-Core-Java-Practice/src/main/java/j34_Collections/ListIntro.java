530a44a79039dc7096fcad3998e81c86a6897641
selimdemirer <selimmdemirerr@gmail.com>
2024-05-11 12:33:38 +0300

    Collections is continuing

--- Java01-Core-Java-Practice/src/main/java/j34_Collections/ListIntro.java
+++ Java01-Core-Java-Practice/src/main/java/j34_Collections/ListIntro.java
@@ -28,14 +28,28 @@ public class ListIntro {
 
         System.out.println("---------------------1---------------------");
 
+        System.out.println(list1);
+        list1.add(70);
+        System.out.println(list1);
+
+        System.out.println("---------------------2---------------------");
+
+        System.out.println(list2);
+        list2.set(5,70); // replacing function
+        System.out.println(list2);
+
+        System.out.println("---------------------3---------------------");
+
+        System.out.println(list3);
+        list3.add(6,70); // inserting function
+        System.out.println(list3);
+
+        System.out.println("---------------------4---------------------");
+
         System.out.println(list4);
-
         ((Stack) list4).pop();
-
         System.out.println(list4);
-
         ((Stack) list4).pop();
-
         System.out.println(list4);
 
     }
